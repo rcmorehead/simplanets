054adc3362069f3aec77603fd504a8e8ac502763
Robert Morehead <rcm242@psu.edu>
2015-10-09 10:42:58 -0400

    added distance function test script

--- abcplot.py
+++ abcplot.py
@@ -10,23 +10,23 @@ import triangle
 import matplotlib
 from scipy.optimize import minimize
 
-plt.style.use('ggplot')
-matplotlib.rcParams['legend.numpoints'] = 1
-matplotlib.rcParams['lines.linewidth'] = 2.0
-matplotlib.rcParams['axes.labelsize'] = 'xx-large'
-matplotlib.rcParams['xtick.labelsize'] = 'xx-large'
-matplotlib.rcParams['ytick.labelsize'] = 'xx-large'
-matplotlib.rcParams['legend.fontsize'] = 'x-large'
-matplotlib.rcParams['xtick.major.size'] = 0
-matplotlib.rcParams[ 'xtick.major.width'] = 2
-matplotlib.rcParams['xtick.minor.size'] = 3
-matplotlib.rcParams['xtick.minor.width'] = 1
-matplotlib.rcParams['ytick.major.size'] = 7
-matplotlib.rcParams['ytick.major.width'] = 2
-matplotlib.rcParams['ytick.minor.size'] = 3
-matplotlib.rcParams['ytick.minor.width'] = 1
-matplotlib.rcParams['figure.subplot.bottom'] = 0.15
-matplotlib.rcParams['savefig.dpi'] = 300
+# plt.style.use('ggplot')
+# matplotlib.rcParams['legend.numpoints'] = 1
+# matplotlib.rcParams['lines.linewidth'] = 2.0
+# matplotlib.rcParams['axes.labelsize'] = 'xx-large'
+# matplotlib.rcParams['xtick.labelsize'] = 'xx-large'
+# matplotlib.rcParams['ytick.labelsize'] = 'xx-large'
+# matplotlib.rcParams['legend.fontsize'] = 'x-large'
+# matplotlib.rcParams['xtick.major.size'] = 0
+# matplotlib.rcParams[ 'xtick.major.width'] = 2
+# matplotlib.rcParams['xtick.minor.size'] = 3
+# matplotlib.rcParams['xtick.minor.width'] = 1
+# matplotlib.rcParams['ytick.major.size'] = 7
+# matplotlib.rcParams['ytick.major.width'] = 2
+# matplotlib.rcParams['ytick.minor.size'] = 3
+# matplotlib.rcParams['ytick.minor.width'] = 1
+# matplotlib.rcParams['figure.subplot.bottom'] = 0.15
+# matplotlib.rcParams['savefig.dpi'] = 300
 
 def loadabc(filename):
     data = pickle.load(file(filename))
@@ -60,7 +60,7 @@ def lookatresults(data, modes, theta=None, vert=False):
 
     if vert == True:
         subplots = [311, 312, 313]
-        figsize = (6,18)
+        figsize = (6, 9)
     else:
         subplots = [131, 132, 133]
         figsize = (15, 3)
@@ -71,6 +71,7 @@ def lookatresults(data, modes, theta=None, vert=False):
 
     thetas = []
     P = data[-1][0]
+    labelpad = 5
 
     for i in xrange(len(P)):
         x = P[i]
@@ -87,32 +88,32 @@ def lookatresults(data, modes, theta=None, vert=False):
         bins=10
     fig = plt.figure(figsize=figsize)
     plt.subplot(subplots[0])
-    plt.title(thetas[0])
+    #plt.title(thetas[0])
     ker = stats.gaussian_kde(P[0])
-    plt.hist(P[0], bins=bins, normed=True, alpha=0.2)
+    plt.hist(P[0], bins=bins, normed=True, alpha=1)
     x = np.linspace(0.0,90,1000)
     plt.plot(x,ker(x))
-    plt.xlabel(r"$\sigma_{mi}$")
+    plt.xlabel(r"$\sigma_{mi}$", labelpad=labelpad)
     if theta != None:
         plt.axvline(theta[0])
 
-    plt.subplot(subplots[1])
-    plt.title(thetas[1])
+    ax1 = plt.subplot(subplots[1])
+    #plt.title(thetas[1])
     ker = stats.gaussian_kde(P[1])
-    plt.hist(P[1], bins=bins, normed=True, alpha=0.2)
+    plt.hist(P[1], bins=bins, normed=True, alpha=1)
     x = np.linspace(0.0,1.0,1000)
     plt.plot(x,ker(x))
-    plt.xlabel(r"$\sigma_{e}$")
+    plt.xlabel(r"$\sigma_{e}$", labelpad=labelpad)
     if theta != None:
         plt.axvline(theta[1])
 
     plt.subplot(subplots[2])
-    plt.title(thetas[2])
+    #plt.title(thetas[2])
     ker = stats.gaussian_kde(P[2])
-    plt.hist(P[2], bins=bins, normed=True, alpha=0.2)
+    plt.hist(P[2], bins=bins, normed=True, alpha=1)
     x = np.linspace(0,20,1000)
     plt.plot(x,ker(x))
-    plt.xlabel(r"$\lambda$")
+    plt.xlabel(r"$\lambda$", labelpad=labelpad)
     if theta != None:
         plt.axvline(theta[2])
 
---
+++ distance_test.py
@@ -0,0 +1,55 @@
+from simpleabc import simple_abc
+import simple_model
+import numpy as np
+import pickle
+from scipy import stats
+import pylab as plt
+import sys
+
+N = 10
+
+stars = pickle.load(file('stars.pkl'))
+model = simple_model.MyModel(stars)
+
+model.set_prior([stats.uniform(0, 90.0),
+                 stats.uniform(0, 1),
+                 stats.uniform(0, 20)])
+
+theta_0 = (10, 0.1, 10)
+obs = model.generate_data(theta_0)
+
+model.set_data(obs)
+
+sum_stat = model.summary_stats(obs)
+
+thetas, distances = [],[]
+
+for n in xrange(N):
+
+    theta = model.draw_theta()
+    synth = model.generate_data(theta)
+    synth_sum = model.summary_stats(synth)
+    distance = model.distance_function(sum_stat, synth_sum)
+
+    thetas.append(theta)
+    distances.append(distance)
+
+
+
+thetas = np.asarray(thetas).T
+
+out = file('{}_distance_test.pkl'.format(sys.argv[1]), 'w')
+pickle.dump([thetas, distances], out)
+out.close()
+
+
+f = plt.figure()
+for i in xrange(thetas.shape[0]):
+    plt.subplot(thetas.shape[0], 1, i+1)
+    plt.plot(thetas[i], distances, 'o')
+    plt.subplots_adjust(hspace=.45)
+    plt.axvline(theta_0[i], color='gray')
+    plt.xlabel(r'$\theta_{}$'.format(i+1))
+    plt.ylabel('Distance')
+
+f.savefig('{}_distance_test.pdf'.format(sys.argv[1]), dpi=300)
